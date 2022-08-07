a4d98ebbeee5add02a327368ac6b6455b7fc4fa6
m_masashi <drift_tms@yahoo.co.jp>
2022-08-07 21:02:03 +0900

    AppのUnitTestを作りかけて停止

---
+++ MLauncherAppTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using MLauncherApp.ViewModels;
+using System.Windows.Input;
+
+namespace MLauncherAppTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var vm = new MainWindowViewModel();
+        }
+    }
+}
