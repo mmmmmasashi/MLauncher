6cbffdc9e350c94df2af5c86cf32dbe9847a21af
m_masashi <drift_tms@yahoo.co.jp>
2022-08-08 22:35:20 +0900

    リファクタ:テストコード整理

--- MLauncherAppTest/MainWindowTest.cs
+++ MLauncherAppTest/MainWindowTest.cs
@@ -10,34 +10,39 @@ namespace MLauncherAppTest
 {
     public class MainWindowTest
     {
+        private Mock<IMessageService> _serviceMoc;
+        private Mock<IRunnerService> _runnerServiceMoc;
+        private Mock<IFilePathRepository> _repositoryMoc;
+        private MainWindowViewModel _vm;
+
+        public MainWindowTest()
+        {
+            _serviceMoc = new Mock<IMessageService>();
+            _runnerServiceMoc = new Mock<IRunnerService>();
+            _repositoryMoc = new Mock<IFilePathRepository>();
+            
+            _vm = new MainWindowViewModel(_serviceMoc.Object, _runnerServiceMoc.Object, _repositoryMoc.Object);
+        }
+
         [Fact]
         public void リストに存在しない名前を検索した時_存在しませんとエラーメッセージが出る()
         {
-            var serviceMoc = new Mock<IMessageService>();
-            var runnerServiceMoc = new Mock<IRunnerService>();
-            var repositoryMoc = new Mock<IFilePathRepository>();
-            var vm = new MainWindowViewModel(serviceMoc.Object, runnerServiceMoc.Object, repositoryMoc.Object);
-            
-            vm.TextBoxText = "not_exist_name";
-            vm.KeyDownCommand.Execute(Key.Enter);
 
-            serviceMoc.Verify(x => x.ShowMessageBox("一致するパスが存在しません"), Times.Once);
+            _vm.TextBoxText = "not_exist_name";
+            _vm.KeyDownCommand.Execute(Key.Enter);
+
+            _serviceMoc.Verify(x => x.ShowMessageBox("一致するパスが存在しません"), Times.Once);
         }
 
         [Fact]
         public void スラッシュlistで全登録済パスを記録したテキストファイルを開く()
         {
-            var messageServiceMoc = new Mock<IMessageService>();
-            var runnerServiceMoc = new Mock<IRunnerService>();
-            var repositoryMoc = new Mock<IFilePathRepository>();
-            repositoryMoc.Setup(repo => repo.FilePath).Returns(new FilePath("path_list.txt"));
+            _repositoryMoc.Setup(repo => repo.FilePath).Returns(new FilePath("path_list.txt"));
 
-            var vm = new MainWindowViewModel(messageServiceMoc.Object, runnerServiceMoc.Object, repositoryMoc.Object);
+            _vm.TextBoxText = "/list";
+            _vm.KeyDownCommand.Execute(Key.Enter);
 
-            vm.TextBoxText = "/list";
-            vm.KeyDownCommand.Execute(Key.Enter);
-
-            runnerServiceMoc.Verify(x => x.Run(new FilePath("path_list.txt")), Times.Once);
+            _runnerServiceMoc.Verify(x => x.Run(new FilePath("path_list.txt")), Times.Once);
         }
     }
 }
