1a66b19d486f929e40931f27673099824e29f62d
m_masashi <drift_tms@yahoo.co.jp>
2022-08-11 22:14:31 +0900

    単体テストを追加

---
+++ MLauncherAppTest/PathListControlViewModelTest.cs
@@ -0,0 +1,31 @@
+﻿using LauncherModelLib;
+using MLauncherApp.ViewModels;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MLauncherAppTest
+{
+    public class PathListControlViewModelTest
+    {
+        [Fact]
+        public void ファイルパスを選択した状態でRunのイベントが飛ぶとそのファイルパスが返される()
+        {
+            var vm = new PathListControlViewModel();
+            vm.SelectedPathItem = new FilePath("filepath.txt");
+
+            vm.RequestClose += new Action<IDialogResult>(result =>
+            {
+                var filePathSelected = result.Parameters.GetValue<FilePath>("SelectedPathItem");
+                Assert.Equal(new FilePath("filepath.txt"), filePathSelected);
+            });
+
+            vm.RunSelectedItemCommand.Execute();
+        }
+
+    }
+}
