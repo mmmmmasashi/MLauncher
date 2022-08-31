c0a523ff695097cd26d0832c275891072c4d5959
m_masashi <drift_tms@yahoo.co.jp>
2022-08-31 22:19:02 +0900

    候補を選択せずにEnterを受け取って動作するように改善

--- MLauncherApp/Views/MainWindow.xaml.cs
+++ MLauncherApp/Views/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using LauncherModelLib.HotKey;
 using MahApps.Metro.Controls;
+using MLauncherApp.ViewModels;
 
 //for hotkey
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interop;
 
 namespace MLauncherApp.Views
@@ -39,6 +41,37 @@ namespace MLauncherApp.Views
         {
             _hotKeyHandle.Dispose();
             base.OnClosed(e);
+        }
+
+        /// <summary>
+        /// FIX ME
+        /// AutoCompleteBoxから、候補選択しない状態でEnterキー入力を取得する方法がこれしか見つけられていない
+        /// できればMVVMにしてコードビハインドを消し去りたい
+        /// 
+        /// 作業時のメモ↓
+        /// - 操作性の改善：選択してない状態でのEnterに対応したい。
+        ///- 例
+        ///    - 候補が1つしかない状態で、候補を選択しなくてもEnterをおしたら実行してほしい
+        ///    - 候補が2個以上ある状態で、候補を選択しなくてもEnterをおしたらリスト表示してほしい
+        ///- メモ
+        ///    - しかしうまくいかない
+        ///    - PreviewKeyDownを直接取ると、文字を確定させるEnterと、確定後の更なるEnterの区別がつかない？
+        ///        - KeyUpだとうまくいった(?)のでこれでいく
+        /// </summary>
+        private void TextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Return)
+            {
+                var vm = this.DataContext as MainWindowViewModel;
+                if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+                {
+                    vm.RunParentCommand.Execute();
+                }
+                else
+                {
+                    vm.RunCommand.Execute();
+                }
+            }
         }
     }
 }
